9ae01188ff787f111dba885ba42e5eb47b47f25c
McCrakken <10561310@my.uvu.edu>
2019-09-04 18:14:08 -0600

    Added PalindromeDetector class and removed placeholder

---
+++ src/main/java/uvu/cs/cs3250/assignments/PalindromeDetector.java
@@ -0,0 +1,14 @@
+package uvu.cs.cs3250.assignments;
+
+public class PalindromeDetector {
+    public static boolean isPalindrome(String s) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("This is a test!");
+        System.out.println("Right now isPalindrome() only returns \"false\" see: " + isPalindrome("val"));
+    }
+}
+
+
