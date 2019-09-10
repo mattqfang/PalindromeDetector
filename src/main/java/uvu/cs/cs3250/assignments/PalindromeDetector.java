e4da03511e596ebc57c62ef5f35003400fa100c0
jordan <frigis.something@gmail.com>
2019-09-09 18:40:07 -0600

    First attempt at completing assignment, but the test needed to import assert libraries.

---
+++ src/main/java/uvu/cs/cs3250/assignments/PalindromeDetector.java
@@ -0,0 +1,20 @@
+package uvu.cs.cs3250.assignments;
+
+public class PalindromeDetector {
+	public static boolean isPalindrome(String s) {
+		if (s == null)
+			return false;
+
+		s = s.replaceAll("\\W", "");
+		s = s.toLowerCase();
+		int len = s.length();
+		
+		for (int i = 0; i <= len / 2; i++) {
+			if (s.charAt(i) != s.charAt(len-1-i)) {
+				return false;
+			}
+		}
+		
+		return true;
+	}
+}
