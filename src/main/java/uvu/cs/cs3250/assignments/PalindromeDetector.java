8cffed3f6a357ad6ea1e35810c2a8506cbf525a8
Dallen Baldwin <dallen.baldwin@gmail.com>
2019-09-09 18:24:53 -0600

    ok. i'm done now.

--- src/main/java/uvu/cs/cs3250/assignments/PalindromeDetector.java
+++ src/main/java/uvu/cs/cs3250/assignments/PalindromeDetector.java
@@ -1,24 +1,27 @@
 package uvu.cs.cs3250.assignments;
 
 // I, Dallen Baldwin, assert that this code was written by myself
-// and not copied from any other students. 
+// and not copied from any other students.
 
 // A palindrome is a word, phrase, number, or other sequences of characters which reads the same backward as forward, such as “madam” or “racecar”. It is not case sensitive.
 
 public class PalindromeDetector {
     public static boolean isPalindrome(String input) {
         if (input == null) return false;
-        
-        String lower = input.toLowerCase();
+
+        // fix the string
         String fixedString = "";
-        for (int i = 0; i < lower.length(); i++) {
-            if (lower.charAt(i) >= 'a' && lower.charAt(i) <= 'z') {
-                fixedString += lower.charAt(i);
+        for (int i = 0; i < input.length(); i++) {
+            if (input.toLowerCase().charAt(i) >= 'a' && input.toLowerCase().charAt(i) <= 'z') {
+                fixedString += input.toLowerCase().charAt(i);
             }
         }
 
-        for (int i = 0; i < fixedString.length() / 2; i++) {// 5 / 2 = 2.5 -> 2, 4 / 2 = 2
-            if (fixedString.charAt(i) != fixedString.charAt(fixedString.length() - 1 - i)) {
+        // check the fixed string
+        int mid = fixedString.length() / 2; // 5 / 2 = 2.5 -> 2, 4 / 2 = 2
+        int last = fixedString.length() - 1;
+        for (int i = 0; i < mid; i++) {
+            if (fixedString.charAt(i) != fixedString.charAt(last - i)) { // check the two sides
                 return false;
             }
         }
