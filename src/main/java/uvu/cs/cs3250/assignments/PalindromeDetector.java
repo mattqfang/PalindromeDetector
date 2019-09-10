a8e66674961e1201cd835856c908320d78e8290d
Dallen Baldwin <dallen.baldwin@gmail.com>
2019-09-09 18:08:04 -0600

    working. but i kind of want to simplify

--- src/main/java/uvu/cs/cs3250/assignments/PalindromeDetector.java
+++ src/main/java/uvu/cs/cs3250/assignments/PalindromeDetector.java
@@ -3,25 +3,26 @@ package uvu.cs.cs3250.assignments;
 // I, Dallen Baldwin, assert that this code was written by myself
 // and not copied from any other students. 
 
-
 // A palindrome is a word, phrase, number, or other sequences of characters which reads the same backward as forward, such as “madam” or “racecar”. It is not case sensitive.
 
 public class PalindromeDetector {
     public static boolean isPalindrome(String input) {
-        String[] characters = input.toLowerCase().split("");
+        if (input == null) return false;
+        
+        String lower = input.toLowerCase();
         String fixedString = "";
-
-        for (int i = 0; i < characters.length; i++) {
-            if (characters[i] && characters[i]) {
-                fixedString += lowerString.at(i);
+        for (int i = 0; i < lower.length(); i++) {
+            if (lower.charAt(i) >= 'a' && lower.charAt(i) <= 'z') {
+                fixedString += lower.charAt(i);
             }
         }
 
-        // switch (input){
-        //     case "null": 
-        //         return false;
-        //     default: return false;
-        // }
-        return false;
+        for (int i = 0; i < fixedString.length() / 2; i++) {// 5 / 2 = 2.5 -> 2, 4 / 2 = 2
+            if (fixedString.charAt(i) != fixedString.charAt(fixedString.length() - 1 - i)) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
