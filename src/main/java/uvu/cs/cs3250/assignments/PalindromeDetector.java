1da8df33f18365a0cf3b9c99a3855729c3cfddd3
McCrakken <10561310@my.uvu.edu>
2019-09-11 17:01:07 -0600

    Remove special characters, ignore case, and return false if null string

--- src/main/java/uvu/cs/cs3250/assignments/PalindromeDetector.java
+++ src/main/java/uvu/cs/cs3250/assignments/PalindromeDetector.java
@@ -1,26 +1,32 @@
 package uvu.cs.cs3250.assignments;
-//TODO: Add regex to remove invalid characters, (anything that's not a letter or a number?)
 
 public class PalindromeDetector {
     static boolean isPalindrome(String s) {
-        String reverseString = StringReverser(s);
-        return s.equals(reverseString);
+        //If 's' is null then return false before attempting to clean and reverse, because it's unnecessary
+        if (s == null){
+            return false;
+        }
+        //Call StringCleaner() to remove all special characters and numbers
+        s = StringCleaner(s);
+
+        //Reverse string with function StringReverser() and compare to original s with equalsIgnoreCase()
+        return s.equalsIgnoreCase(StringReverser(s));
     }
 
+    //Takes initial string and appends to a StringBuilder it then reverses using the .reverse() method.
+    //Converts the reversed StringBuilder variable to a string using toString() and returns it.
     private static String StringReverser(String s){
         StringBuilder reverseString = new StringBuilder();
+
         reverseString.append(s);
         reverseString.reverse();
 
         return reverseString.toString();
     }
 
-    public static void main(String[] args) {
-        System.out.println("This is a test!");
-        System.out.println("Input of \'val\' should return \"false\" let's see: " + isPalindrome("val"));
-        System.out.println("The string \'assa\' should return true, does it... " + isPalindrome("assa"));
-        System.out.println("I'm going to reverse the string: hubba");
-        System.out.println(StringReverser("hubba"));
+    //Return the string calling .replaceAll() passing a regex to remove numbers and special characters
+    private static String StringCleaner(String s){
+        return s.replaceAll("[^a-zA-Z]","");
     }
 }
 
