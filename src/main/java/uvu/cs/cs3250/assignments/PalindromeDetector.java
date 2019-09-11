b470fd0a20ec1f466a3d0479859425f46008d162
jordan <frigis.something@gmail.com>
2019-09-11 17:26:15 -0600

    Finished project

--- src/main/java/uvu/cs/cs3250/assignments/PalindromeDetector.java
+++ src/main/java/uvu/cs/cs3250/assignments/PalindromeDetector.java
@@ -9,11 +9,9 @@ public class PalindromeDetector {
 		s = s.toLowerCase();
 		int len = s.length();
 		
-		for (int i = 0; i <= len / 2; i++) {
-			if (s.charAt(i) != s.charAt(len-1-i)) {
+		for (int i = 0; i <= len / 2; i++)
+			if (s.charAt(i) != s.charAt(len-1-i))
 				return false;
-			}
-		}
 		
 		return true;
 	}
