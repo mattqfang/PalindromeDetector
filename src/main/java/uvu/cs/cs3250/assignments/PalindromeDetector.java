80c18893eb95948dcbb67890ee36b5f27ba39f38
Unknown <skyesladeut@outlook.com>
2019-09-16 17:30:42 -0600

    Deleted Commented out Sections

--- src/main/java/uvu/cs/cs3250/assignments/PalindromeDetector.java
+++ src/main/java/uvu/cs/cs3250/assignments/PalindromeDetector.java
@@ -2,31 +2,7 @@ package uvu.cs.cs3250.assignments;
 
 //Try to do it in a single loop
 public class PalindromeDetector {
-	
-	/*public static void main(String args[]) {
-		System.out.println(isPalindrome("#B%ob"));
-	}
-	public static boolean isPalindrome(String str) {
-		int lastIndex, firstIndex;
-		if(str == null || str == "" || str == " ") {
-			return false;
-		}
-		else{
-			str = str.replaceAll("\\W", "").toLowerCase();
-			lastIndex = str.length()-1; 
-			firstIndex = 0;
-			for(int i = 0; i < str.length(); i++) {
-				if(str.charAt(lastIndex) != str.charAt(firstIndex)) {
-					return false;
-				}
-				lastIndex--;
-				firstIndex++;
-			}
-			return true;
-		}
-	}*/
-	
-	//a way that doesn't use replaceAll()
+
 	public static boolean isPalindrome(String str) {
 		if(str == null || str.isEmpty()) {
 			return false;
