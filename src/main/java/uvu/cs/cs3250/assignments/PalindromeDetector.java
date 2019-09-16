92f3245939c70035da0a487d8cc8aa570e8f8994
Spencer_Thompson <themalewolf@gmail.com>
2019-09-16 17:18:51 -0600

    Cleaned up version

--- src/main/java/uvu/cs/cs3250/assignments/PalindromeDetector.java
+++ src/main/java/uvu/cs/cs3250/assignments/PalindromeDetector.java
@@ -10,37 +10,18 @@ public class PalindromeDetector {
 		}
 		
 		String low = input.toLowerCase().replaceAll("[^a-z]", "");
-		//int word = low.length();
 		String broke[] = low.split("", 0);
 		
-		//int len = broke.length();
-		//int mod =  % 2;
+
 		boolean answer = true;
 		for(int i = 0; i < low.length(); i++){
 			if(!broke[i].equals(broke[low.length() - 1 - i]))
 			{
 				answer = false;
 			}
-			// else {
-			// 	return true;
-			// }
+			
 		}
 		return answer;
-		// if (mod == 0){
-		// 	if (broke[0] == broke[word-1]){
-
-		// 	}
-		// 	else{
-		// 		return false;
-		// 	}
-		// }
-		// if (mod == 1){
-		// 	if (broke[0] == broke[word-1]){
-				
-		// 	}
-		// 	else{
-		// 		return false;
-		// 	}
-		// }
+		
 	}
 }
